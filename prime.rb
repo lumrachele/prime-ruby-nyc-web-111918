83cb36a2c35a6c6b59d46a829bd2308cfeaa4d9b
lumrachele <rlum894@gmail.com>
2018-11-14 16:04:45 -0500

    created prime?() method to test if number is prime

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+#require 'pry'
+
+def prime?(number)
+
+  if number < 2
+    return false
+  end
+  my_array = (2..number-1).to_a
+  my_array.each do |num|
+    if number % num == 0
+#      binding.pry
+      return false
+#    else
+#      return true
+    end
+  end
+  return true
+end
